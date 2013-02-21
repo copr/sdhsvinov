e3ac5ba9f7ce5aee70afc78f3ddcdb576450a910
Martin Koběrský <martinkobersky@gmail.com>
2013-02-21 21:00:22 +0100

    Zrychleni nacteni obrazku

--- static/javascript/scripts.js
+++ static/javascript/scripts.js
@@ -2,11 +2,20 @@
     document.getElementById(id).getElementsByTagName("img")[poradi].src = img;
     document.getElementById(id).getElementsByTagName("img")[poradi].width = width;
 }
+  var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-38700494-1']);
+      _gaq.push(['_trackPageview']);
+
+        (function() {
+             var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+             ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+             var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();
 
 function change(id, width, height, src){
+    document.getElementById(id).height = height;
     document.getElementById(id).src = src;
     document.getElementById(id).width = width;
-    document.getElementById(id).height = height;
 }
 // function change_bgr(id, class_name, color){
 
