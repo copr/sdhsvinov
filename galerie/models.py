15ab8ffe006eaa6a78cd7cff56744579e8a62563
Martin Koběrský <martinkobersky@gmail.com>
2013-02-21 20:52:42 +0100

    Zmena reprezentace objektu v adminovi

--- galerie/models.py
+++ galerie/models.py
@@ -5,22 +5,17 @@ from django.forms import ModelForm
 CHOICES=(
             ('1', 'hasici'),
             ('2', 'mladez'),
-    
 )
 class Fotograf(models.Model):
     jmeno = models.CharField(max_length=100)
     obrazek = models.ImageField(upload_to='photos/Fotografove')
     obrazek_hover = models.ImageField(upload_to='photos/Fotografove')
     url = models.CharField(max_length=200)
-    
-    def unicode(self):
+    def __unicode__(self):
         return unicode("%s" % self.jmeno)
-    
-    
 class Obrazek(models.Model):
     obrazek = models.ImageField(upload_to='photos/galerie')
     kategorie = models.CharField(max_length=20, choices=CHOICES)
-    
 
 #admin.site.register(Fotograf)
 #admin.site.register(Obrazek)
